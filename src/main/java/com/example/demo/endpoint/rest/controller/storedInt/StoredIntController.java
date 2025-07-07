3227bb8b41e8711485b3edc77b24408bb6dd1441
KareeemNomenjanahary12 <hei.nomena.2@gmail.com>
2025-07-07 19:28:07 +0300

    feat: creation of the StoredIntController endpoint and the StoredIntService service

---
+++ src/main/java/com/example/demo/endpoint/rest/controller/storedInt/StoredIntController.java
@@ -0,0 +1,16 @@
+package com.example.demo.endpoint.rest.controller.storedInt;
+import com.example.demo.service.StoredIntService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class StoredIntController {
+    private StoredIntService storedIntService;
+
+    @GetMapping("/stored-int")
+    public int getStoredInd() {
+        return storedIntService.getStoredInt();
+    }
+}
